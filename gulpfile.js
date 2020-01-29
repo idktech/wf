b7e7a71abcda9e475a38cf6b09137de683a34e42
idktech <shogun@Abv.bg>
2020-01-29 18:39:56 +0200

    added updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
-var gulp = require ('gulp'),
-watch = require ('gulp-watch');
+const {watch} = require ('gulp'),
+// watch = require ('gulp-watch');
 
 gulp.task('default', function(){
+    
     console.log("Hello Gulp");
 });
 
 gulp.task('html', function(){
+
     console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task('watch', function(){
+
+    watch('./app/index.html', function(){
+        gulp.task('html');
+    });
 });
